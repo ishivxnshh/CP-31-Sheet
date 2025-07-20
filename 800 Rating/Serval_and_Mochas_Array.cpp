d71edf1164544be5665e48bc79ef97d4ea65f89c
Shivansh Mittal <www.shivansh065@gmail.com>
2025-07-20 22:44:38 +0530

    added

---
+++ 800 Rating/Serval_and_Mochas_Array.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n)     \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+void solve()
+{
+    ll n;
+    cin >> n;
+    iv(v, n);
+    string res = "no";
+    f(i, n)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (__gcd(v[i], v[j]) <= 2)
+            {
+                res = "yes"; break;
+            }
+        }
+    }
+    cout << res << nline;
+}
+
+int main()
+{
+#ifdef PRADY
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef PRADY
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
